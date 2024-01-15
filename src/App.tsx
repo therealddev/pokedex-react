cda53b409a14ea1b261ff023550fab5568d8f94c
therealdev <arayadiego2020@gmail.com>
2024-01-15 15:15:16 -0300

    installed tailwind and fetching first 12 pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+interface Pokemons {
+  name: string;
+  url: string;
+}
+
+interface Pokemon {
+  name: string;
+  id: number;
+  sprites: {
+    front_default: string;
+  }
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const INITIAL_DATA = {"count":1302,"next":"https://pokeapi.co/api/v2/pokemon?offset=12&limit=12","previous":null,"results":[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},{"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"},{"name":"venusaur","url":"https://pokeapi.co/api/v2/pokemon/3/"},{"name":"charmander","url":"https://pokeapi.co/api/v2/pokemon/4/"},{"name":"charmeleon","url":"https://pokeapi.co/api/v2/pokemon/5/"},{"name":"charizard","url":"https://pokeapi.co/api/v2/pokemon/6/"},{"name":"squirtle","url":"https://pokeapi.co/api/v2/pokemon/7/"},{"name":"wartortle","url":"https://pokeapi.co/api/v2/pokemon/8/"},{"name":"blastoise","url":"https://pokeapi.co/api/v2/pokemon/9/"},{"name":"caterpie","url":"https://pokeapi.co/api/v2/pokemon/10/"},{"name":"metapod","url":"https://pokeapi.co/api/v2/pokemon/11/"},{"name":"butterfree","url":"https://pokeapi.co/api/v2/pokemon/12/"}]}
+  
+  const [pokemons, setPokemons] = useState<Pokemon[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const pokemonsData = await Promise.all(
+        INITIAL_DATA.results.map(async (pokemon) => {
+          const response = await fetch(pokemon.url);
+          const pokemonData = await response.json();
+          return pokemonData;
+        })
+      );
+      
+      console.log('pokemonsData: ', pokemonsData)
+      setPokemons(pokemonsData);
+    };
+  
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main >
+
+      <h1 className='underline mb-10'>Pokedex</h1>
+
+      <ul className='list-none grid grid-cols-4 gap-4'>
+        {
+          pokemons?.map(pokemon => (
+            <li key={pokemon.id} className='border'>
+              {pokemon.name}
+
+              <img src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`} alt={`Front default sprite of ${pokemon.name}`} />
+            </li>
+          ))
+        }
+      </ul>
+
+    </main>
   )
+
 }
 
 export default App
---
+++ src/pages/[pokemon].tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+function Pokemon() {
+  return (
+    <div>[pokemon]</div>
+  )
+}
+
+export default Pokemon
